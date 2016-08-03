2c1d3a559ca84734c39d755c6ae6dd4e0491131c
Richard Saunders <r.saunders@zengenti.com>
2016-08-03 17:10:49 +0100

    Navigation toggle

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,1 +1,13 @@
 require("script!./collapsible.js");
+
+$(document).ready(function(){
+
+    $('.js-nav__toggle').on('click', function(e) {
+      $('.drawer').toggleClass('is-visible');
+    });
+
+    $('.js-drawer__close').on('click', function(e) {
+      $('.drawer').toggleClass('is-visible');
+    });
+
+});
